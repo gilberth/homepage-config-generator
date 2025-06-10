f51c5a9d057a71883c3118b29ca5125107efdda7
Gilberth <gilberthcordova@gmail.com>
2025-06-10 15:28:44 -0500

    📊 Add web vitals reporting utility

---
+++ src/reportWebVitals.js
@@ -0,0 +1,14 @@
+// Report Web Vitals utility for performance monitoring
+const reportWebVitals = onPerfEntry => {
+  if (onPerfEntry && onPerfEntry instanceof Function) {
+    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+      getCLS(onPerfEntry);
+      getFID(onPerfEntry);
+      getFCP(onPerfEntry);
+      getLCP(onPerfEntry);
+      getTTFB(onPerfEntry);
+    });
+  }
+};
+
+export default reportWebVitals;
